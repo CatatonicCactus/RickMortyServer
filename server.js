bf7cdc3d1aabb15bd26c314fe1cca19fe5e65e34
Moh-Khosravi <mohammad.khosravi@gmx.de>
2022-02-08 09:51:44 +0100

    add comment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Rick & Morty
 import express from 'express';
 import { MongoClient } from 'mongodb';
 import 'dotenv/config';
@@ -10,27 +11,27 @@ app.use(express.json());
 const uri = process.env.DB;
 
 const client = new MongoClient(uri, {
-useNewUrlParser: true,
-useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 client.connect(async (err) => {
-console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB');
 });
 
 app.get('/favorite', async (req, res) => {
-const collection = client.db('RickMorty').collection('favorites');
-const allFlights = await collection.find({}).toArray();
-res.send(allFlights);
+  const collection = client.db('RickMorty').collection('favorites');
+  const allFlights = await collection.find({}).toArray();
+  res.send(allFlights);
 });
 
 app.post('/addFavorite', async (req, res) => {
-const body = req.body;
-const collection = client.db('RickMorty').collection('favorites');
-const result = await collection.insertOne(body);
-res.send(result);
+  const body = req.body;
+  const collection = client.db('RickMorty').collection('favorites');
+  const result = await collection.insertOne(body);
+  res.send(result);
 });
 
 app.listen(PORT, () => {
-console.log(`Server is running on port ${PORT}`);
-}); 
+  console.log(`Server is running on port ${PORT}`);
+});
